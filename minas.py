0809f9ef5ff78a050882c0d310b753f30b3c9a42
Vitor Sexto Bernardes <vitorsb@gmail.com>
2020-05-15 14:58:53 +0100

    Add timestamp to microclusters

--- minas.py
+++ minas.py
@@ -1,18 +1,10 @@
 import numpy as np
+from time import time
 
 from skmultiflow.core import BaseSKMObject, ClassifierMixin
 
 from sklearn.cluster import KMeans
 
-
-# for debugging
-DEBUG = True
-
-def debug(msg):
-    if DEBUG == True:
-        print(msg)
-
-        
 
 class Minas(BaseSKMObject, ClassifierMixin):
 
@@ -24,45 +16,47 @@ class Minas(BaseSKMObject, ClassifierMixin):
         self.kini = kini
         self.random_state = random_state
 
-        accepted_algos = ['kmeans'] # TODO: define list of accepted algos
+        accepted_algos = ['kmeans']  # TODO: define list of accepted algos
         if cluster_algorithm not in accepted_algos:
             print('Available algorithms: {}'.format(', '.join(accepted_algos)))
         else:
             self.cluster_algorithm = cluster_algorithm
 
-        self.microclusters = [] # list of microclusters
+        self.microclusters = []  # list of microclusters
 
     def fit(self, X, y, classes=None, sample_weight=None):
         self.microclusters = self.offline(X, y)
+        return self
 
     def partial_fit(self, X, y, classes=None, sample_weight=None):
         # TODO
-        pass
+        self.fit(X, y)
+        return self
 
     def predict(self, X):
         """X is an array"""
         # TODO: remove this ugly loop
-        preds = []
+        predictions = []
         for point in X:
             # find closest centroid
             closest_cluster = min(self.microclusters,
                                   key=lambda cl: cl.distance_to_centroid(point))
-            if closest_cluster.is_inside(point):    # classify in this cluster
-                preds.append(closest_cluster.label)
-            else:   # classify as unknown
-                preds.append(-1)
-        return preds
-
+            if closest_cluster.is_inside(point):  # classify in this cluster
+                predictions.append(closest_cluster.label)
+            else:  # classify as unknown
+                predictions.append(0)
+        return np.asarray(predictions)
 
     def predict_proba(self, X):
         # TODO
-        pass 
-
+        pass
 
     def offline(self, X_train, y_train):
 
+        microclusters = []
+        # in offline phase, consider all instances arriving at the same time in the microclusters:
+        timestamp = time()
         if self.cluster_algorithm == 'kmeans':
-            microclusters = []
             for y_class in np.unique(y_train):
                 # subset with instances from each class
                 X_class = X_train[y_train == y_class]
@@ -74,22 +68,21 @@ class Minas(BaseSKMObject, ClassifierMixin):
                 for class_cluster in np.unique(class_cluster_clf.labels_):
                     # get instances in cluster
                     cluster_instances = X_class[class_cluster_clf.labels_ == class_cluster]
-                    
+
                     microclusters.append(
-                        MicroCluster(y_class, cluster_instances)
+                        MicroCluster(y_class, cluster_instances, timestamp)
                     )
 
         return microclusters
-        
+
 
 class MicroCluster(object):
-    
-    def __init__(self,
-                 label,     # the class the microcluster belongs to
-                 instances,
-                 timestamp = 0):
 
-        # TODO: implement timestamp
+    def __init__(self,
+                 label,  # the class the microcluster belongs to
+                 instances,
+                 timestamp=0):
+
         # TODO: remove instances so it doesn't need to be stored in memory
         super(MicroCluster, self).__init__()
         self.label = label
@@ -99,7 +92,7 @@ class MicroCluster(object):
         self.squared_sum = np.square(instances).sum(axis=0)
         self.timestamp = timestamp
 
-        self.centroid = self.linear_sum/self.n
+        self.centroid = self.linear_sum / self.n
         self.radius = self.get_radius()
 
     def __str__(self):
@@ -109,8 +102,8 @@ class MicroCluster(object):
         Linear sum: {self.linear_sum}
         Squared sum: {self.squared_sum}
         Centroid: {self.centroid}
-        Radius: {self.radius}"""
-        # TODO: add timestamp
+        Radius: {self.radius}
+        Timestamp of last change: {self.timestamp}"""
 
     def get_radius(self):
         """Return radius of the subcluster"""
@@ -121,7 +114,7 @@ class MicroCluster(object):
         # )
         # from BIRCH paper:
         return np.sqrt(
-            np.square(self.distance_to_centroid(self.instances)).sum()/self.n
+            np.square(self.distance_to_centroid(self.instances)).sum() / self.n
         )
         # from MINAS paper:
         # factor = 1.1
@@ -129,9 +122,9 @@ class MicroCluster(object):
 
     def distance_to_centroid(self, X):
         """X is a numpy array of instances"""
-        if len(X.shape) == 1:   # X is only one point
+        if len(X.shape) == 1:  # X is only one point
             return np.linalg.norm(X - self.centroid)
-        else:   # X contains several points
+        else:  # X contains several points
             return np.linalg.norm(X - self.centroid, axis=1)
 
     def is_inside(self, X):
